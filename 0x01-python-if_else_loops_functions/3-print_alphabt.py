befcf5c5eb31ad6c82f520025c29bb9303f61978
Kkigathi <Kigathi.g.james@gmail.com>
2022-09-22 00:08:05 +0300

    l-alphabet q

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for c in range(97, 123):
-    if c == 101 or c == 113:
-        continue
-    print("{}".format(chr(c)), end="")
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{:c}".format(i), end='')
