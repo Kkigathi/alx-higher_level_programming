a9c660c5197bccd05b1e1c851078e3a751144842
Kkigathi <Kigathi.g.james@gmail.com>
2022-09-29 23:30:48 +0300

    uniq

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=None):
-    if matrix is None:
-        matrix = []
+def square_matrix_simple(matrix=[]):
     return [list(map(lambda n: n**2, row)) for row in matrix]
---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    return sum(set(my_list))
+#set does not conatin duplicate values it is mutable but not ordered
