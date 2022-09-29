c60b6cc7cf8f89180000e1b456b9b4bec5939ea7
Kkigathi <Kigathi.g.james@gmail.com>
2022-09-29 23:13:28 +0300

    matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    return [list(map(lambda n: n**2, sublist)) for sublist in matrix]
+def square_matrix_simple(matrix=None):
+    if matrix is None:
+        matrix = []
+    return [list(map(lambda n: n**2, row)) for row in matrix]
