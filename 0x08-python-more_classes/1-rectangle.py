f82418422ac3297d736c12e29e0ea5eae984b86a
Kkigathi <Kigathi.g.james@gmail.com>
2022-10-17 21:57:10 +0300

    classes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-
+"""
+This is the "Rectangle"  module.
+This module provides a simple Rectangle class with attribute width and height.
+Default values of both attributes are 0.
+"""
 class Rectangle:
     """A Rectangle class with attributes  width and height"""
     def __init__(self, width=0, height=0):
